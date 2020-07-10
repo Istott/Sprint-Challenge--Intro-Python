ef4bb3e3c84757fc2b24d76bde255ac77f3dce3d
isaac stott <isaac.cloyd@gmail.com>
2020-07-10 11:39:33 -0600

    got the hypen comprehension working

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -47,7 +47,8 @@ print(d)
 # Write a list comprehension that creates a list of strings which are the name
 # joined to the age with a hyphen, for example "David-31", for all humans.
 print("Name hyphen age:")
-e = ['-'.join(n) for n in humans ]
+# hyphenThem = '-' + str(Human.age)
+e = [''.join(n.name + '-') + str(n.age) for n in humans ]
 print(e)
 
 # Write a list comprehension that creates a list of tuples containing name and
