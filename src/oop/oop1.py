069fde479ec7b9122fbc1351fe464dbce2443c6f
isaac stott <isaac.cloyd@gmail.com>
2020-07-10 09:15:38 -0600

    got assignment setup. got started on oop1

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,34 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+#my parent class
+class Vehicle():
+    def __init__(self, name):
+        self.name = name
+
+class GroundVehicle(Vehicle):
+    def __init__(self, name, make):
+        super().__init__(name)
+        self.make = make
+
+class Car(GroundVehicle):
+    def __init__(self, name, make):
+        super().__init__(name, make)
+
+class Motorcycle(GroundVehicle):
+    def __init__(self, name, make):
+        super().__init__(name, make)
+
+
+class FlightVehicle():
+    def __init__(self):
+        super().__init__()
+
+class Airplane(FlightVehicle):
+    def __init__(self):
+        super().__init__()
+
+class Starship():
+    def __init__(self):
+        super().__init__()
